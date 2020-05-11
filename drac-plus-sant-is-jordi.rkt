3b9fed607205d9ba505f61c73c5a8a8b8598be8b
Jos Koot <jos.koot@gmail.com>
2020-05-11 16:44:45 +0200

    Add files via upload

--- drac-plus-sant-is-jordi.rkt
+++ drac-plus-sant-is-jordi.rkt
@@ -1,133 +1,154 @@
 #lang racket
 
+; By Jacob J. A. Koot, 2020
+
+(require "../fmt/fmt.rkt") ; Nothing provided.
+
 ;----------------------------------------------------------------------------------------------------
 
-(display
-"
- -----------------------------
- Sant Jordi, día 23 de febrero
- -----------------------------
+(define header
+ "
+ ┌────────────────────────────────────┐
+ │ Sant Jordi, día 23 de febrero 2020 │
+ └────────────────────────────────────┘
 
- Todas las permutaciones  (A C D I J N Ø R S T)
+ Todas las permutaciones  (A C D I J N O R S T)
  de los dígitos decimales (0 1 2 3 4 5 6 7 8 9) tal que:
 
        DRAC
        SANT
-    + -----
-      JØRDI
+    + ─────
+      JORDI
 
  Permutaciones con J=0 incluidas o excluidas.
- Tenga en cuenta que J=0 o J=1, nunca J>1.
-
- ‘Ø’ es la letra mayúscula decimoquinta del alfabeto latino.
- ‘0’ es el dígito cero.\n\n")
+ Tenga en cuenta que J=0 o J=1, nunca J>1. \n\n")
 
 ;----------------------------------------------------------------------------------------------------
 
-(require "fmt/fmt.rkt") ; Nothing provided.
-(define (fmt-cur . fmts) (apply fmt 'cur fmts))
-(define 0-9 (in-range 0 10))
-(define digits '(0 1 2 3 4 5 6 7 8 9))
-(define significances #(1 10 100 1000 10000))
-(define (find-decimal-digit n i) (modulo (quotient n (vector-ref significances i)) 10))
-(define (J=1? solution) (>= (caddr solution) 10000))
-
-;----------------------------------------------------------------------------------------------------
-; solutions = ((DRAC SANT JØRDI (A C D I J N Ø R S T)) ...)
-; in which (A C D I J N Ø R S T) is a permutation of (0 1 2 3 4 5 6 7 8 9) and DRAC + SANT = JØRDI.
-;----------------------------------------------------------------------------------------------------
-
-(define solutions ; J=0 included.
+(define (make-list-of-solutions) ; J=0 included.
  (sort
-  (for*/list
-   ((A (in-list digits)) (digits (in-value (remove A digits)))
-    (C (in-list digits)) (digits (in-value (remove C digits)))
-    (D (in-list digits)) (digits (in-value (remove D digits)))
-    (N (in-list digits)) (digits (in-value (remove N digits)))
-    (R (in-list digits)) (digits (in-value (remove R digits)))
-    (S (in-list digits)) (digits (in-value (remove S digits)))
-    (T (in-list digits))
+  (for*/list ; nr of iterations: 10!/3! = 604800, the ones excluded by the when-clause included.
+   ((combination (in-combinations '(0 1 2 3 4 5 6 7 8 9) 7))
+    (permutation (in-permutations combination))
+    ((A C D N R S T) (in-values (apply values permutation)))
     (DRAC  (in-value (+ (* 1000 D) (* 100 R) (* 10 A) C)))
     (SANT  (in-value (+ (* 1000 S) (* 100 A) (* 10 N) T)))
-    (JØRDI (in-value (+ DRAC SANT)))
-    (occupied-digits (in-value (list A C D N R S T)))
-    (J (in-value (find-decimal-digit JØRDI 4)))
-    (Ø (in-value (find-decimal-digit JØRDI 3)))
-    (I (in-value (find-decimal-digit JØRDI 0)))
+    (JORDI (in-value (+ DRAC SANT)))
+    (J (in-value (find-digit JORDI 10000)))
+    (O (in-value (find-digit JORDI 1000)))
+    (I (in-value (find-digit JORDI 1)))
     #:when
     (and
-     (not (check-duplicates (list J Ø I) =))
-     (not (member J occupied-digits =))
-     (not (member Ø occupied-digits =))
-     (not (member I occupied-digits =))
-     (= (find-decimal-digit JØRDI 1) D) 
-     (= (find-decimal-digit JØRDI 2) R)))
-   (list DRAC SANT JØRDI (list A C D I J N Ø R S T)))
-  (λ (x y)
-   (or (< (caddr x) (caddr y))
-    (and (= (caddr x) (caddr y))
-         (< (cadr x) (cadr y)))))))
+     (= (find-digit JORDI 10) D)
+     (= (find-digit JORDI 100) R)
+     (not (check-duplicates (list J O I) =))
+     (not (member J combination =))
+     (not (member O combination =))
+     (not (member I combination =))))
+   (list DRAC SANT JORDI (list A C D I J N O R S T)))
+  solution<?))
 
 ;----------------------------------------------------------------------------------------------------
 
 (define (display-solutions J=0-included?)
- (let ((solutions (if J=0-included? solutions (filter J=1? solutions))))
- 
-  ((fmt-cur "' ----------------------------'/
-             ' Soluciones con J=0 ' Q'incluidas' 'excluidas' S/
-             ' ----------------------------'//") J=0-included?) 
- 
-  (define JØRDIS (map caddr solutions))
- 
-  (if J=0-included?
-   ((fmt-cur "' Hay ' I2 ' soluciones, ' I2 ' con J=1 y ' I2 ' con J=0.'/")
-    (length solutions)
-    (count (λ (n) (> n  9999)) JØRDIS)
-    (count (λ (n) (< n 10000)) JØRDIS))
-   ((fmt-cur "' Hay ' I2 ' soluciones.'/") (length solutions)))
- 
-  ((fmt-cur "/ X 'DRAC + SANT = JØRDI' 3X 'A C D I J N Ø R S T' //
-             U#(XUS I4.4 ' + ' I4.4 ' = ' I5.5 2X U#I2/)/")
-   solutions)
- 
-  ((fmt-cur "' Valores que asumen los dígitos:'//"))
- 
-  (for ((i 0-9))
-       (let ((letter (string-ref "ACDIJNØRST" i))
-             (digits (sort (remove-duplicates (map (λ (x) (list-ref (cadddr x) i)) solutions) =) <)))
-            ((fmt-cur "XD' :'U#I2/") letter digits)))
- 
-  (newline)
- 
-  ((fmt-cur "' Número de soluciones en las el dígito'/
-             ' A, C, D, I, J, N, Ø, R, S o T tiene el valor'/
-             ' 0, 1, 2, 3, 4, 5, 6, 7, 8 o 9.'//"))
- 
-  (define line (fmt "' |-------|-------------------------------|'/"))
- 
-  ((fmt-cur line "' | valor |  A  C  D  I  J  N  Ø  R  S  T |'/" line))
- 
-  (define (erase-zero x) (if (zero? x) "   " ((fmt "I3") x)))
- 
-  (let ((f (fmt-cur "' | 'I5' |'US10D ' |'/")))
-   (for ((i 0-9))
-    (let ((lst (for/list ((j 0-9)) (count (λ (x) (= (list-ref (cadddr x) j) i)) solutions))))
-     (f i (map erase-zero lst)))))
- 
-  ((fmt-cur line
-    "/"
-    "' Cada columna y cada fila suma a ' D ','/"
-    "' es decir, al número total de soluciones.'/"
-    "' Esto está evidente para las columnas.'/"
-    "' También se aplica a las filas porque cada solución'/"
-    "' contiene cada dígito exactamente una vez.'//")
-   (length solutions))))
+
+ (define numbered-solutions
+  (for/list
+   ((n (in-naturals 1))
+    (solution (in-list solutions))
+    #:when (or J=0-included? (J=1? solution)))
+    (cons n solution)))
+
+ (define JORDIS (map fourth numbered-solutions))
+
+ (fmt0 J=0-included?)
+
+ (if J=0-included?
+  (fmt1
+   (length numbered-solutions)
+   (count (λ (JORDI) (> JORDI  9999)) JORDIS)
+   (count (λ (JORDI) (< JORDI 10000)) JORDIS))
+  (fmt2 (length numbered-solutions)))
+
+ (fmt-3-4-5a numbered-solutions)
+
+ (for ((i 0-9))
+  (let ((lst (for/list ((j 0-9)) (count (λ (x) (= (list-ref (digits x) j) i)) numbered-solutions))))
+   (fmt5b i (map erase-zero lst))))
+
+ (fmt-5c-6 (length numbered-solutions)))
 
 ;----------------------------------------------------------------------------------------------------
 
+(define line0a (fmt "' ┌──────────────────────────────┐\n'"))
+(define line0b (fmt "' └──────────────────────────────┘\n'"))
+(define line3a (fmt "'    ┌────┬─────────────────────┬─────────────────────┐\n'"))
+(define line3b (fmt "'    ├────┼─────────────────────┼─────────────────────┤\n'"))
+(define line3c (fmt "'    └────┴─────────────────────┴─────────────────────┘\n'"))
+(define line5a (fmt "'    ┌───────┬───────────────────────────────┐\n'"))
+(define line5b (fmt "'    ├───────┼───────────────────────────────┤\n'"))
+(define line5c (fmt "'    └───────┴───────────────────────────────┘\n'"))
+(define fmt0 (fmt 'cur line0a "' │ Soluciones con J=0 ' Q'incluidas │' 'excluidas │' S/" line0b "/"))
+(define fmt1 (fmt 'cur "' Hay 'I2' soluciones, 'I2' con J=1 y 'I2' con J=0:'/"))
+(define fmt2 (fmt 'cur "' Hay ' I2 ' soluciones:'/"))
+(define fmt5a (fmt 'cur line5a "'    │ valor │  A  C  D  I  J  N  O  R  S  T │'/" line5b))
+(define fmt5b (fmt 'cur "'    │ 'I5' │'US10D' │'/"))
+(define fmt5c (fmt 'cur line5c "/"))
+(define digits fifth)
+(define jordi caddr)
+(define sant cadr)
+(define fmtXI2 (fmt "XI2"))
+(define (erase-zero x) (if (zero? x) "   " (fmtXI2 x)))
+(define 0-9 (in-range 0 10))
+(define (find-digit n significance) (modulo (quotient n significance) 10))
+(define (J=1? solution) (>= (third solution) 10000))
+
+(define fmt3
+ (fmt 'cur
+  "/" line3a
+  "'    │ nr │ DRAC + SANT = JORDI │ A C D I J N O R S T │'/"
+  line3b
+  "U#('    │ 'USI2' │ 'I4.4' + 'I4.4' = 'I5.5' │'U#I2' │'/)"
+  line3c "/"))
+
+(define fmt4
+ (fmt 'cur
+  "' Número de soluciones en las el dígito'/
+   ' A, C, D, I, J, N, O, R, S o T tiene el valor'/
+   ' 0, 1, 2, 3, 4, 5, 6, 7, 8 o 9:'//"))
+
+(define fmt6
+ (fmt 'cur
+  "' Cada columna y cada fila suma a 'D','/"
+  "' es decir, al número total de soluciones.'/"
+  "' Esto está evidente para las columnas.'/"
+  "' También se aplica a las filas porque cada solución'/"
+  "' contiene cada dígito exactamente una vez.'//"))
+
+(define fmt-3-4-5a (fmt 'cur fmt3 fmt4 fmt5a))
+(define fmt-5c-6 (fmt 'cur fmt5c fmt6))
+
+(define (solution<? x y)
+ (let ((xj (jordi x)) (yj (jordi y)))
+  (or (< xj yj) (and (= xj yj) (< (sant x) (sant y))))))
+
+(define-sequence-syntax in-values
+ (λ (stx) (raise-syntax-error 'in-values "can be used in for forms only" stx))
+ (λ (stx)
+  (syntax-case stx ()
+   (((id ...) (_ expr))
+  #'((id ...) (:do-in (((id ...) expr)) #t () #t () #t #f ()))))))
+
+;----------------------------------------------------------------------------------------------------
+
+(define solutions (make-list-of-solutions))
+(display header)
 (display-solutions #t)
 (display-solutions #f)
 
 ;----------------------------------------------------------------------------------------------------
 
-((fmt-cur "' El fin'//"))
+((fmt 'cur
+  "' ┌────────┐'/"
+  "' │ El fin │'/"
+  "' └────────┘'//"))
